29d3d0945c93570f9eeb1cc4057a594d5ef83566
Ichtiostega <51170359+Ichtiostega@users.noreply.github.com>
2021-11-26 15:53:20 +0100

    Finished lab4

--- lab4_3.m
+++ lab4_3.m
@@ -1,6 +1,7 @@
 %% load
 close all; clc;
-squares = imread('squares.bmp');
+%squares = imread('squares.bmp');
+squares = imread('jet.bmp');
 load('masks');
 %% Roberts
 figure(1);
@@ -20,5 +21,12 @@ laplace_norm(squares, S2);
 %% combined filter
 C1 = conv2(squares, S1, 'same');
 C2 = conv2(squares, S2, 'same');
-squares_comb = (C1.^2 + C2.^2)^(1/2);
+squares_comb = sqrt((C1.^2 + C2.^2));
+sum_comb = abs(C1) + abs(C2);
+figure(7);
+subplot(1,3,1);
+imshow(squares);
+subplot(1,3,2);
 imshow(squares_comb, []);
+subplot(1,3,3);
+imshow(sum_comb, []);
